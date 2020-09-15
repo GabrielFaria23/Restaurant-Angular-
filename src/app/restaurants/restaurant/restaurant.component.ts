5453132e163688944c4a1340d3194db6846b1efc
GabrielFaria23 <gabrielfariapta01@gmail.com>
2020-09-15 11:13:56 -0300

    Criacao: header, home, restaurants(restaurant) e config de rotas

---
+++ src/app/restaurants/restaurant/restaurant.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { RestaurantsComponent } from '../restaurants.component';
+import { Restaurant } from './restaurant.model';
+
+@Component({
+  selector: 'mt-restaurant',
+  templateUrl: './restaurant.component.html'
+})
+export class RestaurantComponent implements OnInit {
+
+  @Input() restaurant: Restaurant;
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log(this.restaurant);
+  }
+
+}
