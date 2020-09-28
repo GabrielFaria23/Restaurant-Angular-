9acdafd2ec5227b9fd892ea34ccd051c0645317a
GabrielFaria23 <gabrielfariapta01@gmail.com>
2020-09-28 08:57:11 -0300

    Tratamento de erros com catch

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -6,6 +6,9 @@ import { RESTAURANT_API } from './../../app/app.api';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+import { ErrorHandler } from '../app.error-handler'
 
 @Injectable()
 export class RestaurantsService{
@@ -13,8 +16,9 @@ export class RestaurantsService{
     constructor(private http: Http){}
 
     restaurants(): Observable<Restaurant[]> {
-        return this.http.get(`${RESTAURANT_API}/restaurants`)
+        return this.http.get(`${RESTAURANT_API}/restaurants1`)
             .map(response => response.json())
+            .catch(ErrorHandler.handleError)
     }
 
 }
