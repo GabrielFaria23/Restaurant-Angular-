137f5e761a3b8a0c70b9550f8381e07da71b6087
GabrielFaria23 <gabrielfariapta01@gmail.com>
2020-09-29 15:53:35 -0300

    Tela de conclusão de compra implementada

---
+++ src/app/order-summary/order-summary.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-order-sumary',
+  templateUrl: './order-summary.component.html'
+})
+export class OrderSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { CartItem } from 'app/restaurants/restaurant-detail/shopping-cart/cart.item.model';
 import { RadioOption } from 'app/shared/radio/radio.Option.model';
 import { Order, OrderItem } from './order.model';
@@ -18,7 +19,8 @@ export class OrderComponent implements OnInit {
     {label: 'Cartão Refeição', value: "REF"}
   ]
 
-  constructor(private orderService: OrderService) { }
+  constructor(private orderService: OrderService,
+              private router: Router) { }
 
   ngOnInit() {
   }
@@ -48,7 +50,7 @@ export class OrderComponent implements OnInit {
     .map((item:CartItem)=> new OrderItem(item.quantity, item.menuItem.id)) //convertendo array de cartItem para orderItem
     this.orderService.checkOrder(order)
       .subscribe( (orderId: string) => {
-        console.log(`Compra concluída: ${orderId}`)
+        this.router.navigate(['/order-summary'])
         this.orderService.clear()
       })
   }
