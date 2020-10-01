7996b287eef4eb2c30ce9666bb35243b3d524f5e
GabrielFaria23 <gabrielfariapta01@gmail.com>
2020-10-01 15:15:09 -0300

    Usando pipe, tap, timer e switchMap para corrigir sneakbar

--- src/app/shared/messages/snackbar/snackbar.component.ts
+++ src/app/shared/messages/snackbar/snackbar.component.ts
@@ -2,6 +2,8 @@ import { state, style, trigger, animate, transition } from '@angular/animations'
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/timer'
+import 'rxjs/add/operator/switchMap'
+import 'rxjs/add/operator/do'
 import { NotificationService } from '../notification.service';
 
 @Component({
@@ -32,11 +34,25 @@ export class SnackbarComponent implements OnInit {
   constructor(private notificationService: NotificationService) { }
 
   ngOnInit() {
-    this.notificationService.notifier.subscribe(message =>{
+    this.notificationService.notifier.subscribe(message =>{ //subscribe coloca um listener no ponto desejado e so apartir desse ponto ele notifica, do permite executar uma ação no momento em que recebe a mensagem
       this.message = message
       this.snackVisibility = 'visible'
-      Observable.timer(3000).subscribe(timer=> this.snackVisibility = 'hidden')
+      Observable.timer(3000).subscribe(timer=> this.snackVisibility = 'hidden') // espera 3 segundos e depois some a mensagem 
     })
   }
 
+  /*ngOnInit() {
+    this.notificationService.notifier.pipe(
+    tap( message => { //subscribe coloca um listener no ponto desejado e so apartir desse ponto ele notifica, do permite executar uma ação no momento em que recebe a mensagem
+      this.message = message
+      this.snackVisibility = 'visible'
+      //Observable.timer(3000).subscribe(timer=> this.snackVisibility = 'hidden') // espera 3 segundos e depois some a mensagem 
+    }),
+       switchMap(message => timer(30000))
+    ).subscribe(timer=> this.snackVisibility = 'hidden')
+  } maneira correta, mas pipe não funciona*/
+
 }
+
+
+//swithMap troca o observable inteiro, ou seja da pra trocar os eventos que emitiria a partir do ponto
