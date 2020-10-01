878b488718567f062c85c4692d36d3f4ee5c55dd
GabrielFaria23 <gabrielfariapta01@gmail.com>
2020-10-01 15:34:33 -0300

    Adicionando animacoes no menuItem dos restaurantes

--- src/app/restaurants/restaurant-detail/menu-item/menu-item.component.ts
+++ src/app/restaurants/restaurant-detail/menu-item/menu-item.component.ts
@@ -1,11 +1,23 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MenuItem } from './menu-item.model';
 
 @Component({
   selector: 'mt-menu-item',
-  templateUrl: './menu-item.component.html'
+  templateUrl: './menu-item.component.html',
+  animations: [
+    trigger('menuItemAppeared', [
+      state('ready', style({opacity: 1})),
+      transition('void => ready', [
+        style({opacity: 0, transform: 'translate(-10px)'}), // eixo x -30px e eixo y - 10 px
+        animate('500ms 0s ease-in')
+      ])
+    ])
+  ]
 })
 export class MenuItemComponent implements OnInit {
+
+  menuItemState: string = 'ready'
 
   @Input() menuItem: MenuItem
   @Output() add = new EventEmitter()
