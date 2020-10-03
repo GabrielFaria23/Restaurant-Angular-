23aa87f9229a3197a303f2bab3c37798ec515dbb
GabrielFaria23 <gabrielfariapta01@gmail.com>
2020-10-03 12:38:16 -0300

    LoginService created (correct)

--- src/app/security/login/login.service.ts
+++ src/app/security/login/login.service.ts
@@ -6,11 +6,19 @@ import { User } from "./user.model";
 
 @Injectable()
 export class LoginService {
+
+    user: User
     constructor(private http:HttpClient){}
+
+    isLoggedIn(): boolean {
+        return this.user !== undefined
+    }
 
     login(email: string, password:string): Observable<User>{
         return this.http.post<User>(`${RESTAURANT_API}/login`, 
-            {email: email, password: password})
+                                {email: email, password: password})
+                        .do(user => this.user = user)
+
     }
 
     //assistir aula do fim pra usar o pipe no lugar do 'do'
