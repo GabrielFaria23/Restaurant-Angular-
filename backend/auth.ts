9e97a8d4eaf0b02ba9d8966db5c5bcc6bc7e05ef
GabrielFaria23 <gabrielfariapta01@gmail.com>
2020-10-03 11:25:07 -0300

    Adicionando Token JWT a Rota de login

--- backend/auth.ts
+++ backend/auth.ts
@@ -1,11 +1,14 @@
 import {Request, Response} from 'express'
 import { User, users } from './users'
 
+import * as jwt from 'jsonwebtoken'
+
 export const handleAuthentication = (req: Request, resp: Response)=>{
     const user = req.body
     if(isValid(user)){
         const dbUser: User = users[user.email]
-        resp.json({name: dbUser.name, email:dbUser.email})
+        const token = jwt.sign({sub: dbUser.email, iss: 'meat-api'}, 'meat-api-password') //sub: portador do token iss: quem esta emitindo o token, password para assinar o token (não compartilhaa token com cliente assim fica mais seguro)
+        resp.json({name: dbUser.name, email:dbUser.email, accessToken: token})
     }else{
         resp.status(403).json({message: 'Dados inválidos.'})
     }
--- backend/dist/auth.js
+++ backend/dist/auth.js
@@ -1,11 +1,13 @@
 "use strict";
 exports.__esModule = true;
 var users_1 = require("./users");
+var jwt = require("jsonwebtoken");
 exports.handleAuthentication = function (req, resp) {
     var user = req.body;
     if (isValid(user)) {
         var dbUser = users_1.users[user.email];
-        resp.json({ name: dbUser.name, email: dbUser.email });
+        var token = jwt.sign({ sub: dbUser.email, iss: 'meat-api' }, 'meat-api-password'); //sub: portador do token iss: quem esta emitindo o token, password para assinar o token (não compartilhaa token com cliente assim fica mais seguro)
+        resp.json({ name: dbUser.name, email: dbUser.email, accessToken: token });
     }
     else {
         resp.status(403).json({ message: 'Dados inválidos.' });
