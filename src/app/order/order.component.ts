8fbd61adbceb568d918a80dd8c961915a103ab24
GabrielFaria23 <gabrielfariapta01@gmail.com>
2020-10-06 08:29:27 -0300

    Formularios atualizados (blur)

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CartItem } from 'app/restaurants/restaurant-detail/shopping-cart/cart.item.model';
 import { RadioOption } from 'app/shared/radio/radio.Option.model';
@@ -33,15 +33,18 @@ export class OrderComponent implements OnInit {
               private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.orderForm = this.formBuilder.group({
-      name: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+    this.orderForm = new FormGroup({
+      name: new FormControl ('', {
+        validators: [Validators.required, Validators.minLength(5)],
+        updateOn: 'blur' //agora o campo é validade so quando perde o foco
+      }), //essa maneira é utilizada para validar cada form separadamente 
       email: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
       emailConfirmation: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
       address: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
       number: this.formBuilder.control('', [Validators.required, Validators.pattern(this.numberPattern)]),
       optionalAddress: this.formBuilder.control(''),
       paymentOption: this.formBuilder.control('', [Validators.required])
-    }, {validator: OrderComponent.equalsTo})
+    }, {validators: [OrderComponent.equalsTo], updateOn: 'blur'}) //dessa maneira aplica o blur no form todo sem precisar fazer configurações em cada campo separadamente
   }
 
   static equalsTo(group: AbstractControl): {[key:string]: boolean}{
